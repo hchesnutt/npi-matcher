bbacd274988e3283a5c642e26356eff0c53d28ce
hchesnutt <t.henry.chesnutt@gmail.com>
2018-07-26 11:53:54 -0700

    move columns file and refactor client to only contain client info

--- columns.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  '(^NPI$',
-  '|^Provider First Name$',
-  '|^Provider Middle Name$',
-  '|^Provider Last Name',
-  '|^Provider Organization Name',
-  '|^Provider Gender Code$)',
-].join('');
